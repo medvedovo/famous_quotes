40a2d1018910cc203c07e93db37f3aa04205a976
Пронин Евгений <e.pronin@litebox.ru>
2018-09-22 15:48:49 +0400

    Реализована возможность делиться цитатами

--- app/src/main/java/ru/medvedovo/famousquotes/ui/MainActivity.kt
+++ app/src/main/java/ru/medvedovo/famousquotes/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.medvedovo.famousquotes.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -17,15 +18,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         model = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        initObservers()
+        initHandlers()
+    }
+
+    private fun initObservers() {
         model.getQuote().observe(this, Observer {
             text_quote_main.text = it.quoteText
             text_quote_author.text = if (!it.quoteAuthor.isNullOrEmpty())
                 String.format(Locale.getDefault(), getString(R.string.quote_author), it.quoteAuthor)
-                    else ""
+            else ""
         })
+    }
 
+    private fun initHandlers() {
         fab_refresh.setOnClickListener {
             model.loadQuote()
+        }
+
+        fab_share.setOnClickListener {
+            val quote = model.getQuote().value ?: return@setOnClickListener
+
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                if (quote.quoteAuthor.isNullOrEmpty()) {
+                    putExtra(Intent.EXTRA_TEXT,
+                            String.format(Locale.getDefault(), getString(R.string.quote_share_format_noauthor), quote.quoteText))
+                } else {
+                    putExtra(Intent.EXTRA_TEXT,
+                            String.format(Locale.getDefault(), getString(R.string.quote_share_format_author), quote.quoteText, quote.quoteAuthor))
+
+                }
+                type = "text/plain"
+            }
+            startActivity(shareIntent)
         }
     }
 }
